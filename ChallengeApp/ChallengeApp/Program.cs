eb28c9cf070e0d7e596420d9b7bc1bde531a40d8
YuriiYusko <yura.yusko.y@gmail.com>
2023-03-04 18:15:12 +0100

    Day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// My first App
+
+String name = "Ewa";
+bool isWoman = true;
+int age = 31;
+
+if (!isWoman){
+    if (age < 18){
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else{
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
+else if (age < 30){
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else{
+    Console.WriteLine(name + ", lat " + age);
+}
