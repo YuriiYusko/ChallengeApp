6c38d8abf4a4e594852c0305cb6e341b5aac99a0
YuriiYusko <yura.yusko.y@gmail.com>
2023-03-06 21:22:02 +0100

    Day 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,73 @@
 ﻿// My first App
 
-String name = "Ewa";
-bool isWoman = true;
-int age = 31;
+//-Tablice i ich użycie
 
-if (!isWoman){
-    if (age < 18){
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-    else{
-        Console.WriteLine("Pełnoletni mężczyzna");
+int[] grades = new int[365];
+string[] dayOfWeeks = new string[7];
+dayOfWeeks[0] = "Monday";
+dayOfWeeks[1] = "Tuesday";
+dayOfWeeks[2] = "Wednesday";
+dayOfWeeks[3] = "Thursday";
+dayOfWeeks[4] = "Friday";
+dayOfWeeks[5] = "Saturday";
+dayOfWeeks[6] = "Sunday";
+
+//Console.WriteLine(dayOfWeeks[0]);
+
+string[] dayOfWeeks2 = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+//Console.WriteLine(dayOfWeeks[dayOfWeeks.Count()-1]);
+
+//-Pętla for
+//-Połączenie tablic i pętli for
+
+for (int i = 0; i < dayOfWeeks.Length; i++)
+{
+    //Console.WriteLine(dayOfWeeks[i]);
+}
+
+//-Listy
+
+List<string> dayOfWeeks3 = new List<string>();
+dayOfWeeks3.Add("Monday");
+dayOfWeeks3.Add("Tuesday");
+dayOfWeeks3.Add("Wednesday");
+dayOfWeeks3.Add("Thursday");
+dayOfWeeks3.Add("Friday");
+dayOfWeeks3.Add("Saturday");
+dayOfWeeks3.Add("Sunday");
+
+for (int i = 0; i < dayOfWeeks3.Count; i++)
+{
+    //Console.WriteLine(dayOfWeeks3[i]);
+}
+
+//-Pętla foreach    
+foreach (string day in dayOfWeeks3)
+{
+    //Console.WriteLine(day);
+}
+
+//-Zadanie Domowe
+
+int number = 654646321;
+string numberStr = number.ToString();
+char[] letteres = numberStr.ToCharArray();
+
+int[] numberCount = new int[10];
+
+
+foreach (char num in letteres)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        int numInt = num - '0';
+        if (numInt == i) { numberCount[i] = numberCount[i] + 1; }
     }
 }
-else if (age < 30){
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else{
-    Console.WriteLine(name + ", lat " + age);
+
+Console.WriteLine("Wyniki dla liczby: " + numberStr);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i +  " => " + numberCount[i]);
 }
