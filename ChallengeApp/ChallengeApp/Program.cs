89b47ecb7c96b28b95b79adf2d31e049fa8d76b3
YuriiYusko <yura.yusko.y@gmail.com>
2023-03-07 20:59:32 +0100

    Day 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,73 +1,17 @@
 ﻿// My first App
 
-//-Tablice i ich użycie
+//Dzień 6: Klasy i metody w klasie
 
-int[] grades = new int[365];
-string[] dayOfWeeks = new string[7];
-dayOfWeeks[0] = "Monday";
-dayOfWeeks[1] = "Tuesday";
-dayOfWeeks[2] = "Wednesday";
-dayOfWeeks[3] = "Thursday";
-dayOfWeeks[4] = "Friday";
-dayOfWeeks[5] = "Saturday";
-dayOfWeeks[6] = "Sunday";
+using ChallengeApp;
 
-//Console.WriteLine(dayOfWeeks[0]);
+User user0 = new User("Adam", "74794");
+User user1 = new User("Yurii", "18303");
+User user2 = new User("Mariana", "19452");
+User user3 = new User("Zuzi", "853013");
 
-string[] dayOfWeeks2 = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+user0.AddScore(5);
+user0.AddScore(3);
 
-//Console.WriteLine(dayOfWeeks[dayOfWeeks.Count()-1]);
-
-//-Pętla for
-//-Połączenie tablic i pętli for
-
-for (int i = 0; i < dayOfWeeks.Length; i++)
-{
-    //Console.WriteLine(dayOfWeeks[i]);
-}
-
-//-Listy
-
-List<string> dayOfWeeks3 = new List<string>();
-dayOfWeeks3.Add("Monday");
-dayOfWeeks3.Add("Tuesday");
-dayOfWeeks3.Add("Wednesday");
-dayOfWeeks3.Add("Thursday");
-dayOfWeeks3.Add("Friday");
-dayOfWeeks3.Add("Saturday");
-dayOfWeeks3.Add("Sunday");
-
-for (int i = 0; i < dayOfWeeks3.Count; i++)
-{
-    //Console.WriteLine(dayOfWeeks3[i]);
-}
-
-//-Pętla foreach    
-foreach (string day in dayOfWeeks3)
-{
-    //Console.WriteLine(day);
-}
-
-//-Zadanie Domowe
-
-int number = 654646321;
-string numberStr = number.ToString();
-char[] letteres = numberStr.ToCharArray();
-
-int[] numberCount = new int[10];
-
-
-foreach (char num in letteres)
-{
-    for (int i = 0; i < 10; i++)
-    {
-        int numInt = num - '0';
-        if (numInt == i) { numberCount[i] = numberCount[i] + 1; }
-    }
-}
-
-Console.WriteLine("Wyniki dla liczby: " + numberStr);
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine(i +  " => " + numberCount[i]);
-}
+Console.WriteLine(user0.Result);
+var name = User.gameName;
+Console.WriteLine(Math.PI);
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp
+{
+    public class User
+    {
+        public static string gameName = "Diablo";
+        private List<int> score;
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.score = new List<int>();
+        }
+
+        public string Login { get; private set; }
+
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get { return this.score.Sum(); }
+        }
+
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }
+}
